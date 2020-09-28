d4b770440eca5fac5fd15219ec9ec3f3cf9fe7c9
Brakkau <matt.egan@UKMANMAC185.local>
2020-09-28 14:46:41 +0100

    INIT

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('addition of numbers', () => {
+  const wrapper = render(<App />);
+});
+
+
